12c99ae950e865fa6142e689426d995c41cd7d80
Elchin <elchin@datorium.eu>
2023-10-26 18:16:53 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace WinFormsApp_WASD_controls_2023_2024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
